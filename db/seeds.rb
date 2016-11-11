03754ae209fc691c4680961d6c81c4928c6139b9
Conrad Kirby <conrad.kirby@gmail.com>
2016-11-11 13:33:08 -0500

    add user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  name:     'user1',
+  password: 'pass1',
+  email:    'user1@example.com'
+)
