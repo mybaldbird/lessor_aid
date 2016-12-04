f1f33d022edaef5710929578e06dc2bb68ac9409
Conrad Kirby <conrad.kirby@gmail.com>
2016-12-03 23:09:08 -0500

    app_nav: add style

--- app/assets/javascripts/components/app_nav.vue
+++ app/assets/javascripts/components/app_nav.vue
@@ -1,21 +1,69 @@
+<style lang="sass">
+  .navbar {
+    background: rgb(39, 40, 34);
+    color: rgb(230, 219, 116);
+    height: 3em;
+    span {
+      font-size: 1.5em;
+      float: left;
+    }
+    ul {
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      li {
+        height: 2em;
+        line-height: 1.75em;
+        float: left;
+      }
+    }
+    ul.left {
+      float: left;
+      li {
+        margin-left: 1em;
+      }
+    }
+    ul.right {
+      float: right;
+      li {
+        margin-right: 1em;
+      }
+    }
+    a:link, a:visited {
+      color: rgb(102, 217, 239);
+    }
+    a:hover {
+      color: rgb(249, 88, 164);
+    }
+    a:active {
+      color: rgb(249, 38, 114);
+    }
+    div.clear {
+      clear: both;
+    }
+  }
+</style>
+
 <template>
-  <div>
+  <div class="navbar">
     <span>LessorAid</span>
     <template v-if="signedIn">
-      <ul>
+      <ul class="left">
         <li>nav1</li>
         <li>nav2</li>
         <li>nav3</li>
       </ul>
-      <ul>
+      <ul class="right">
         <li>Welcome {{ currentUser.name }}</li>
         <li><a href="javascript:void(0);" @click="signOut">sign out</a></li>
       </ul>
     </template>
-    <ul v-else>
+    <ul class="right" v-else>
       <li><router-link to="/signin">sign in</router-link></li>
       <li><router-link to="/join">sign up</router-link></li>
     </ul>
+    <div class="clear"></div>
   </div>
 </template>
 
